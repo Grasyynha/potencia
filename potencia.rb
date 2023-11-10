7257daf6c29a7b44288cb832c9468e8bb45887b5
Grasiela Texeira <grasy.mst@gmail.com>
2023-11-10 17:14:11 -0300

    Criado o projeto para o curso de Ruby da DIO

---
+++ potencia.rb
@@ -0,0 +1,17 @@
+# Cria um array vazio
+numeros = []
+
+# Solicita ao usuário que insira 3 números
+for i in 1..3 do
+  puts "Digite o número #{i}: "
+  numero = gets.chomp.to_i
+  numeros << numero
+end
+
+# Seleciona os números que serão elevados à terceira potência
+numeros = numeros.select { |numero| numero.positive? }
+
+# Exibe o resultado dos números elevados à terceira potência
+numeros.each do |numero|
+  puts "#{numero} elevado a terceira potência é: #{numero ** 3}"
+end
